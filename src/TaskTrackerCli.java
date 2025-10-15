79f1495d9a9f65563eef16c34d4b330a6002cbf7
jamesmayke <jamezmayke28@gmail.com>
2025-10-15 14:31:34 -0300

    feat(cli): add TaskTrackerCli to handle user commands

--- src/TaskTrackerCli.java
+++ src/TaskTrackerCli.java
@@ -1,5 +1,33 @@
-public class Main {
+import java.io.IOException;
+
+public class TaskTrackerCli {
     public static void main(String[] args) {
-        
+        TaskManager taskManager = new TaskManager();
+
+        if (args.length < 1) {
+            System.out.println("Use argumentos para executar o programa");
+        }
+
+        String commando = args[0];
+
+        switch (commando) {
+            case "add":
+                if (args.length < 2) {
+                    System.out.println("Usage: TaskCLIApp add <description>");
+                    return;
+                }
+                taskManager.addTask(args[1]);
+                break;
+            case "list":
+                System.out.println("Listando tasks...");
+                System.out.println(taskManager.listTasks());
+                break;
+            default:
+                System.out.println("Comando desconhecido: " + commando);
+
+        }
+
+        taskManager.saveTask();
+
     }
 }
